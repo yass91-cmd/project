03bc9ba73530ed864211d9fa0d7ae5eb4739c9e1
yasser <boudrigayasser1@gmail.com>
2025-04-06 16:15:02 +0100

    tri function

--- name.py
+++ name.py
@@ -1,8 +1,9 @@
-def fib(n):
-    if n==0:
-        return 0
-    elif n==1:
-        return 1
-    else:
-        return  fib(n-1) + fib(n-2)
-print(fib(6))
+def tri(l):
+
+    for i in range(len(l)):
+        for j in range(len(l)-i-1):
+            if l[i]>l[j+1]:
+                l[j],l[j+1]=l[j+1],l[j]
+l = [5, 2, 9, 1]
+tri(l)
+print(l)
